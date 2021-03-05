6a07adb034e769006e856d48037055453a04e183
ShadowfaxRodeo <nathanielcaird@Nathaniels-MacBook-Pro.local>
2021-03-05 20:47:18 +0000

    testing oEmbed description

--- src/_data/sites.js
+++ src/_data/sites.js
@@ -7,7 +7,9 @@ module.exports = [
             metadescription: false,
             microdata: false,
             jsonld: false,
-            oembed_xml: false,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
             oembed_json_rich: false,
             oembed_json_photo: null,
             oembed_json_link: null,
@@ -34,7 +36,9 @@ module.exports = [
             metadescription: true,
             microdata: false,
             jsonld: false,
-            oembedxml: false,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
             oembed_json_rich: null,
             oembed_json_photo: null,
             oembed_json_link: null,
@@ -53,10 +57,12 @@ module.exports = [
                 works: false,
                 note: "double check this | odd that the microdata format works"
             },
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
             oembed_xml_rich: null,
-            oembed_json_rich: null,
-            oembed_json_photo: null,
-            oembed_json_link: null,
+            oembed_json_rich: false,
+            oembed_json_photo: false,
+            oembed_json_link: false,
             opengraph: true,
             twitter: true
         },
@@ -73,10 +79,19 @@ module.exports = [
             metadescription: true,
             microdata: false,
             jsonld: false,
-            oembed_xml_rich: null,
-            oembed_json_rich: null,
-            oembed_json_photo: null,
-            oembed_json_link: null,
+            oembed_xml_rich: false,
+            oembed_json_rich: {
+                works: true,
+                note: "Does not show thumbnail."
+            },
+            oembed_json_photo: {
+                works: true,
+                note: "Does not show photo."
+            },
+            oembed_json_link: {
+                works: true,
+                note: "Does not show thumbnail."
+            },
             opengraph: true,
             twitter: true,
         }
@@ -85,25 +100,148 @@ module.exports = [
         name: "facebook",
         url: "https://facebook.com",
         tests: {
-            metadescription: {
-                works: true
-            }
+            title: null,
+            metadescription: null,
+            microdata: null,
+            jsonld: null,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: null,
+            oembed_json_photo: null,
+            oembed_json_rich: null,
+            twitter: null,
+            opengraph: null
         }
     },
-
+    {
+        name: "Whatsapp",
+        url: "",
+        tests: {
+            title: null,
+            metadescription: null,
+            microdata: null,
+            jsonld: null,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: null,
+            oembed_json_photo: null,
+            oembed_json_rich: null,
+            twitter: null,
+            opengraph: null
+        }
+    },
+    {
+        name: "Signal",
+        url: "",
+        tests: {
+            title: null,
+            metadescription: null,
+            microdata: null,
+            jsonld: null,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: null,
+            oembed_json_photo: null,
+            oembed_json_rich: null,
+            twitter: null,
+            opengraph: null
+        }
+    },
+    {
+        name: "Signal",
+        url: "https://www.signal.org/",
+        tests: {
+            title: true,
+            metadescription: true,
+            microdata: false,
+            jsonld: false,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: false,
+            oembed_json_photo: false,
+            oembed_json_rich: false,
+            twitter: false,
+            opengraph: true
+        }
+    },
+    {
+        name: "Telegram",
+        url: "",
+        tests: {
+            title: {
+                works: true,
+                note: "in conjunction with a meta description"
+            },
+            metadescription: true,
+            microdata: true,
+            jsonld: false,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: false,
+            oembed_json_photo: false,
+            oembed_json_rich: false,
+            twitter: true,
+            opengraph: true
+        }
+    },
+    {
+        name: "Discourse",
+        url: "https://www.discourse.org/",
+        tests: {
+            title: true,
+            metadescription: false,
+            microdata: false,
+            jsonld: false,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: true,
+            oembed_json_photo: false,
+            oembed_json_rich: true,
+            twitter: true,
+            opengraph: true
+        }
+    },
+    {
+        name: "reddit",
+        url: "https://www.reddit.com/",
+        tests: {
+            title: null,
+            metadescription: null,
+            microdata: null,
+            jsonld: null,
+            oembed_xml_photo: null,
+            oembed_xml_link: null,
+            oembed_xml_rich: null,
+            oembed_json_link: null,
+            oembed_json_photo: null,
+            oembed_json_rich: null,
+            twitter: null,
+            opengraph: null
+        }
+    }
 ]
 
-// {
-//     name: "",
-//     url: "",
-// tests:{
-//     title: -,
-//     metadescription: -,
-//     microdata: -,
-//     jsonld: -,
-//     oembedxml: -,
-//     oembedjson: -,
-//     twitter: -,
-//     opengraph: -
-// }
-// },
+var test = {
+    name: "",
+    url: "",
+    tests: {
+        title: null,
+        metadescription: null,
+        microdata: null,
+        jsonld: null,
+        oembed_xml_photo: null,
+        oembed_xml_link: null,
+        oembed_xml_rich: null,
+        oembed_json_link: null,
+        oembed_json_photo: null,
+        oembed_json_rich: null,
+        twitter: null,
+        opengraph: null
+    }
+}
