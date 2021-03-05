f816fdb0558b27d5902de232cebb0f4d7ef454ac
ShadowfaxRodeo <nathanielcaird@Nathaniels-MacBook-Pro.local>
2021-03-05 16:15:30 +0000

    switch oEmbed to use absolute urls

--- src/_data/sites.js
+++ src/_data/sites.js
@@ -3,26 +3,50 @@ module.exports = [
         name: "twitter",
         url: "https://twitter.com",
         tests: {
-            metadescription: {
-                works: false
+            title: false,
+            metadescription: false,
+            microdata: false,
+            jsonld: false,
+            oembedxml: false,
+            oembedjson: false,
+            twitter: {
+                works: true,
+                note: "must use absolute urls"
             },
-            microdata: {
-                works: false
-            },
-            jsonld: {
-                works: false
+            opengraph: {
+                works: true,
+                note: "must use absolute urls"
             }
 
         }
 
     },
     {
+        name: "slack",
+        url: "",
+        title: {
+            works: true,
+            note: "only in conjunction with metadescription"
+        },
+        metadescription: true,
+        microdata: false,
+        jsonld: false,
+        oembedxml: false,
+        oembedjson: {
+            works: false,
+            note: "double check this"
+        },
+        twitter: true,
+        opengraph: true
+    },
+    {
         name: "facebook",
         url: "https://facebook.com",
         tests: {
             metadescription: {
-                status: "good"
+                works: true
             }
         }
-    }
+    },
+
 ]
